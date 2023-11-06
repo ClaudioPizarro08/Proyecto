ee2c12335ad72c60b7bce5d80eaa0a81ae05d01d
ClaudioPizarro08 <claudio155219@hotmail.com>
2023-11-06 07:48:15 -0300

    Creacion de Rutas y páginas

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import { Navbar } from "../Navbar"
+import { HomePage, LoginPage, RegisterPage, DashboardPage } from "../pages"
+
+
+export const AppRouter = () => {
+  return <>
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+            <Route index element={<HomePage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="dashboard" element={<DashboardPage />} />
+      </Routes>
+  </>
+}
